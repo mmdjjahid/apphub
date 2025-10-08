364a32434c314c816ca08bb1091b1e8a3ebbbc42
mmdjjahid <mmdjjahid@gmail.com>
2025-10-08 11:40:19 +0600

    router setup done

---
+++ src/components/routes/routes.jsx
@@ -0,0 +1,12 @@
+import { createBrowserRouter } from "react-router";
+import Home from "../../pages/home";
+
+
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Home
+  },
+]);
+
